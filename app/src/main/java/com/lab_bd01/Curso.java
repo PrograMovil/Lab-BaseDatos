abfe2fb43c1200918208ea564af5b1e948fc96cd
SheshoVega <hvega.sergio@gmail.com>
2017-05-28 12:24:21 -0600

    cambio de SDK y model clases

---
+++ app/src/main/java/com/lab_bd01/Curso.java
@@ -0,0 +1,44 @@
+package com.lab_bd01;
+
+/**
+ * Created by SheshoVega on 28/05/2017.
+ */
+
+public class Curso {
+    int id;
+    String nombre;
+    String descripcion;
+    int creditos;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+}
---
+++ app/src/main/java/com/lab_bd01/Estudiante.java
@@ -0,0 +1,53 @@
+package com.lab_bd01;
+
+/**
+ * Created by SheshoVega on 28/05/2017.
+ */
+
+public class Estudiante {
+    int id;
+    String nombre;
+    String apellido1;
+    String apellido2;
+    int edad;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
