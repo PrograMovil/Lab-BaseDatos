b8514b20543a5e8b69c0ed54fc7618d39c425bd9
SheshoVega <hvega.sergio@gmail.com>
2017-05-30 00:06:25 -0600

    mejorando home

--- app/src/main/java/com/lab_bd01/MainActivity.java
+++ app/src/main/java/com/lab_bd01/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setTitle("Cursos y Estudiantes");
 
         btnCrudCur=(Button) findViewById(R.id.botonCrudCursos);
         btnCrudEst=(Button) findViewById(R.id.botonCrudEstudiantes);
