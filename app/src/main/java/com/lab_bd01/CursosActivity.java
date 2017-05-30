ccc38c41626adb97c17778d57c0201e9ac4b5236
SheshoVega <hvega.sergio@gmail.com>
2017-05-29 23:39:08 -0600

    arreglar los fragments again -.-

--- app/src/main/java/com/lab_bd01/CursosActivity.java
+++ app/src/main/java/com/lab_bd01/CursosActivity.java
@@ -1,5 +1,7 @@
 package com.lab_bd01;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,14 @@ public class CursosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cursos);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentCursosContainer);
+        if (fragment == null) {
+            fragment = new CursosFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentCursosContainer, fragment)
+                    .commit();
+        }
     }
 }
