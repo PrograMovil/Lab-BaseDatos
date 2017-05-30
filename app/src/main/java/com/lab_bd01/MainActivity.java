00c7b7099f90a79b28f1b78479674bca1685e03e
Denis <vite0150@gmail.com>
2017-05-29 22:51:27 -0600

    ejecutar fragments desde boton

--- app/src/main/java/com/lab_bd01/MainActivity.java
+++ app/src/main/java/com/lab_bd01/MainActivity.java
@@ -1,9 +1,11 @@
 package com.lab_bd01;
 
+        import android.content.Intent;
         import android.database.sqlite.SQLiteDatabase;
         import android.database.sqlite.SQLiteException;
         import android.support.v4.app.Fragment;
         import android.support.v4.app.FragmentManager;
+        import android.support.v4.app.FragmentTransaction;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.view.View;
@@ -24,19 +26,12 @@ public class MainActivity extends AppCompatActivity {
         btnCrudCur=(Button) findViewById(R.id.botonCrudCursos);
         btnCrudEst=(Button) findViewById(R.id.botonCrudEstudiantes);
 
+
         btnCrudCur.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Cargar fragment de la lista de cursos
-                FragmentManager fm = getSupportFragmentManager();
-                Fragment fragment = fm.findFragmentById(R.id.fragmentCursosContainer);
-
-                if (fragment == null) {
-                    fragment = new CursosFragment();
-                    fm.beginTransaction()
-                            .add(R.id.fragmentCursosContainer, fragment)
-                            .commit();
-                }
+                Intent intent = new Intent(MainActivity.this, CursosActivity.class);
+                MainActivity.this.startActivity(intent);
             }
         });
 
@@ -44,16 +39,8 @@ public class MainActivity extends AppCompatActivity {
         btnCrudEst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Cargar fragment de la lista de estudiantes
-                FragmentManager fm = getSupportFragmentManager();
-                Fragment fragment = fm.findFragmentById(R.id.fragmentEstudiantesContainer);
-
-                if (fragment == null) {
-                    fragment = new EstudiantesFragment();
-                    fm.beginTransaction()
-                            .add(R.id.fragmentEstudiantesContainer, fragment)
-                            .commit();
-                }
+                Intent intent = new Intent(MainActivity.this, EstudiantesActivity.class);
+                MainActivity.this.startActivity(intent);
             }
         });
 
